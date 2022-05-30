c34fe029630c016b11821117cde12cde235537ef
Mahmoud Samir <menatosamir55@gmail.com>
2022-05-31 01:52:07 +0200

    adding review validator

--- Routes/review.routes.js
+++ Routes/review.routes.js
@@ -6,21 +6,21 @@ const {
   updateReview,
   deleteReview,
 } = require("../controllers/review.controller");
-// const {
-//   getBrandValidator,
-//   createBrandValidator,
-//   updateBrandValidator,
-//   deleteBrandValidator,
-// } = require("../utils/validators/brandValidator");
+const {
+  getReviewValidator,
+  createReviewValidator,
+  updateReviewValidator,
+  deleteReviewValidator,
+} = require("../utils/validators/reviewValidator");
 
 const { auth, allowedTo } = require("../controllers/auth.controller");
 
-router.route("/").get(getReviews).post(auth, allowedTo("user"), createReview);
+router.route("/").get(getReviewValidator, getReviews).post(auth, allowedTo("user"), createReviewValidator, createReview);
 
 router
   .route("/:id")
   .get(getReview)
-  .put(auth, allowedTo("user"), updateReview)
-  .delete(auth, allowedTo("user", "manager", "admin"), deleteReview);
+  .put(auth, allowedTo("user"), updateReviewValidator, updateReview)
+  .delete(auth, allowedTo("user", "manager", "admin"), deleteReviewValidator, deleteReview);
 
 module.exports = router;
