de77ad87dbee10e1d6bd450403bc42f66351ec13
Mahmoud Samir <menatosamir55@gmail.com>
2022-06-07 01:56:18 +0200

    push to heroku

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.DB_URI, () => {
-    console.log(`Connected to db on port ${process.env.DB_URI}`);
+    console.log(`Connected to db`);
 })
