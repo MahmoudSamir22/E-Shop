fada03202c3dc06e9c765ce6b8651cdddc1073d4
Mahmoud Samir <menatosamir55@gmail.com>
2022-11-20 18:38:56 +0200

    branch check

--- server.js
+++ server.js
@@ -74,6 +74,6 @@ process.on("unhandledRejection", (err) => {
   console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}`);
   server.close(() => {
     console.error("Shutting down....");
-    process.exit(1);
+    process.exit(1)
   });
 });
